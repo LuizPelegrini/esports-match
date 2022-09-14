9de0075f544995581ac1b3c39101fe354d2f5751
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2022-09-14 22:47:38 +0800

    update: Scaffold games and ads endpoints

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,12 +2,32 @@ import express from 'express';
 
 const app = express();
 
-app.get('/ads', (request, response) => {
-    console.log('Hello');
+// List games
+app.get('/games', (request, response) => {
+    return response.json([]);
+});
 
+// List game ads
+app.get('/games/:id/ads', (request, response) => {
+    return response.json([]);
+});
+
+// Fetch discord from an ad
+app.get('/ads/:id/discord', (request, response) => {
+    return response.json([]);
+});
+
+// List ads
+app.get('/ads', (request, response) => {
     return response.json({ message: 'ok' });
 });
 
+// Create an ad
+app.post('/ads', (request, response) => {
+    return response.status(201).json([]);
+});
+
+// Init server
 app.listen(3333, () => {
     console.log('Server started at 3333! 🚀');
 });
