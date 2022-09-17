3cc920d264a15553fc8d968c90058851ce1ea1cc
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2022-09-17 15:13:32 +0800

    update: Fetch list of games from backend

--- web-client/src/App.tsx
+++ web-client/src/App.tsx
@@ -1,10 +1,30 @@
+import { useState, useEffect } from 'react';
 import './styles/main.css';
 import logoImg from './assets/logo-esports.svg';
 
 import { GameCard } from './components/GameCard';
 import { CreateAdBanner } from './components/CreateAdBanner';
 
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+}
+
 function App() {
+  // initialize game list
+  const [games, setGames] = useState<Game[]>([]);
+
+  // useEffect without dependencies runs only once (even if component state changes)
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+      .then(response => response.json())
+      .then(data => setGames(data))
+  }, []);
+
   return (
     <div className='max-w-[1334px] mx-auto flex flex-col items-center my-20'>
       <img src={logoImg} />
@@ -14,7 +34,16 @@ function App() {
       </h1>
 
       <div className='grid grid-cols-6 gap-6 mt-16'>
-        <GameCard bannerUrl='./game-1.png' title='League of Legends' adsCount={3}/>
+        { 
+          games.map(game => 
+            <GameCard
+              key={game.id}
+              bannerUrl={game.bannerUrl}
+              title={game.title}
+              adsCount={game._count.ads}
+            />
+          ) 
+        }
       </div>
 
       <CreateAdBanner />
